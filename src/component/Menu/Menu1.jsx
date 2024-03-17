398710dede56cc7d0946f7332d7fa463a9c52021
shrasht971 <shrashtisahu5@gmail.com>
2024-03-17 21:07:17 +0530

    menu1 page

--- src/component/Menu/Menu1.jsx
+++ src/component/Menu/Menu1.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import Menu1first from "../../assets/image/menu1first.png";
 import Burger from "../../assets/image/burger.jpeg";
 import Chinese from "../../assets/image/chinese.jpeg";
@@ -11,48 +12,95 @@ import Combo from "../../assets/image/combo.png";
 import Beverage from "../../assets/image/bestOffer4.jpeg";
 
 const Menu1 = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleReadMoreLess = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <div className="bg-menu1 lg:h-[1800px]   overflow-x-hidden ">
-      <div className=" relative top-28 max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 ">
-        {/* <div className="container border-red-800 border-2  ">
+      <div className=" relative top-28 p-1 m-1">
+        <div className="container ">
           <img src={Menu1first} className="h-96 w-full whitespace-pre object-cover " />
-        </div> */}
-        {/* <div className="flex w-full">
-          <div className="bg-white ml-6 w-[555px] h-px border-0 float-left mt-8"></div>
-          <div className="text-2xl text-white m-4 tracking-widest font-semibold ">
+        </div>
+        <div className="flex w-full">
+          <div className="bg-white ml-6 w-[540px] h-px border-0  mt-10"></div>
+          <div className="text-2xl text-white my-6 mx-4  tracking-widest font-semibold ">
             WHAT’S ON YOUR MIND?
           </div>
-          <div className="bg-white mr-6 w-[550px] h-px border-0 float-right mt-8"></div>
-        </div> */}
-        {/* <div className="w-full">
+          <div className="bg-white mr-6 w-[535px] h-px border-0  mt-10"></div>
+        </div>
+        <div className="w-full">
           <div className="flex justify-evenlyely m-2  ">
-            <img src={Burger} className="w-40 h-40 rounded-full mx-32 my-16" />
-            <img src={Chinese} className="w-40 h-40 rounded-full mx-32 my-16" />
+          <div className=" mx-28 my-16 ">
+            <img src={Burger} className="w-52 h-52 rounded-full ml-8 " />
+            <p className="text-2xl text-white font-semibold text-center mt-2">BURGER & SANDWICHES</p>
+          </div>  
+          <div className=" mx-36 my-16">
+            <img src={Chinese} className="w-52 h-52 rounded-full" />
+            <p className="text-2xl text-white font-semibold text-center  mt-2">CHINESE CUSINE</p>
+          </div>
+          <div className="mx-28 my-16">
             <img
               src={Starters}
-              className="w-40 h-40 rounded-full mx-32 my-16"
+              className="w-52 h-52 rounded-full "
             />
+            <p className="text-2xl text-white font-semibold text-center  mt-2">STARTERS</p>
+            </div>
           </div>
           <div className="flex justify-evenly m-2 ">
-            <img src={Sides} className="w-40 h-40 rounded-full mx-32 my-16" />
-            <img src={Salads} className="w-40 h-40 rounded-full mx-32 my-16" />
-            <img
-              src={Desserts}
-              className="w-40 h-40 rounded-full mx-32 my-16"
-            />
+          <div className=" mx-28 my-16">
+            <img src={Sides} className="w-52 h-52 rounded-full " />
+            <p className="text-2xl text-white font-semibold text-center  mt-2">SIDES</p>
+            </div>
+            <div className=" mx-28 my-16">
+            <img src={Salads} className="w-52 h-52 rounded-full" />
+            <p className="text-2xl text-white font-semibold text-center  mt-2">SALADS</p>
+            </div>
+            <div className="mx-28 my-16">
+            <img src={Desserts} className="w-52 h-52 rounded-full " />
+             <p className="text-2xl text-white font-semibold text-center  mt-2">DESSERTS / ICE-CREAM</p>
+          </div>
           </div>
           <div className="flex justify-evenly m-2 ">
-            <img
-              src={MainCoure}
-              className="w-40 h-40 rounded-full mx-32 my-16"
-            />
-            <img src={Combo} className="w-40 h-40 rounded-full mx-32 my-16" />
-            <img
-              src={Beverage}
-              className="w-40 h-40 rounded-full mx-32 my-16"
-            />
+          <div className="mx-28 my-16">
+            <img src={MainCoure} className="w-52 h-52 rounded-full " />
+            <p className="text-2xl text-white font-semibold text-center  mt-2">MAIN COURSE</p>
+            </div>
+            <div className=" mx-28 my-16">
+            <img src={Combo} className="w-52 h-52 rounded-full" />
+            <p className="text-2xl text-white font-semibold text-center  mt-2">value combo</p>
+            </div>
+            <div className="mx-28 my-16">
+            <img src={Beverage} className="w-52 h-52 rounded-full " />
+            <p className="text-2xl text-white font-semibold text-center  mt-2">BEVERAGES</p>
+            </div>
           </div>
-        </div> */}
+          {isOpen && (
+          <div className="flex justify-evenly m-2 ">
+          <div className=" mx-28 my-16">
+            <img src={Sides} className="w-52 h-52 rounded-full " />
+            <p className="text-2xl text-white font-semibold text-center  mt-2">SIDES</p>
+            </div>
+            <div className=" mx-28 my-16">
+            <img src={Salads} className="w-52 h-52 rounded-full" />
+            <p className="text-2xl text-white font-semibold text-center  mt-2">SALADS</p>
+            </div>
+            <div className="mx-28 my-16">
+            <img src={Desserts} className="w-52 h-52 rounded-full " />
+             <p className="text-2xl text-white font-semibold text-center  mt-2">DESSERTS / ICE-CREAM</p>
+          </div>
+          </div>
+          )}
+          <div className="flex justify-center">
+          <button
+              className="text-black text-xs px-4 py-2 lg:text-lg rounded-xl border-b lg:w-52 lg:h-18  lg:px-2 lg:py-4 text-center font-bold uppercase tracking-wider hover:bg-red-900 lg:mb-8 lg:ml-11"
+              onClick={toggleReadMoreLess}
+            >
+              {isOpen ? "Read Less" : "See More"}
+            </button>
+            </div>
+        </div>
       </div>
     </div>
   );
